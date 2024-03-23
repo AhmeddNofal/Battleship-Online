1d2064f90e3993bfa0594e6a8cb84a3d5122a542
AhmeddNofal <52462958+AhmeddNofal@users.noreply.github.com>
2024-03-23 15:46:46 +0200

    Add files via upload

---
+++ host/Player.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp3
+{
+    class Player
+    {
+        public List<string> Ships = new List<string>();
+        public int Score { get; set; }
+
+        public bool turn { get; set; }
+        
+    }
+}
